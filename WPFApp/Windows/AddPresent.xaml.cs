d042589a357650b809d5d969594005a4a83de007
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 19:35:46 +0200

    Add AddPresent Window

---
+++ WPFApp/Windows/AddPresent.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using EFLib.Services;
+
+namespace WPFApp.Windows;
+
+public partial class AddPresent : Window
+{
+    private readonly IPresentService _service;
+    public AddPresent()
+    {
+        InitializeComponent();
+        _service = new PresentService();
+    }
+
+    private void AddPresent_Button(object sender, RoutedEventArgs e)
+    {
+        if (Date != null && ChildrenId != null && Presents != null )
+        {
+            Boolean present;
+            if ((bool) Presents.IsChecked)
+            {
+                present = true;
+            }
+            else
+            {
+                present = false;
+            }
+            
+            _service.Create(Date.SelectedDate.Value, Int32.Parse(ChildrenId.Text), present);
+            MessageBox.Show("Added successfully!");
+            this.Close();
+        }
+        else
+        {
+            MessageBox.Show("Invalid input!");
+        }
+    }
+}
