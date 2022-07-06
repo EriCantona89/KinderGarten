030d51d2e9b987876f80d196b686f2297124b901
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 18:37:30 +0200

    Add TeacherService and ITeacherService

---
+++ EFLib/Services/ITeacherService.cs
@@ -0,0 +1,12 @@
+﻿using EFLib.Models;
+
+namespace EFLib.Services;
+
+public interface ITeacherService
+{
+    void Create(string firstName,string lastName);
+    void Update(int id, string firstName,string lastName);
+    void Delete(int id);
+    Teacher GetById(int id);
+    List<Teacher> GetAll();
+}
--- EFLib/Services/TeacherService.cs
+++ EFLib/Services/TeacherService.cs
@@ -1,6 +1,47 @@
-﻿namespace EFLib.Services;
+﻿using EFLib.Models;
 
-public class TeacherService
+namespace EFLib.Services;
+
+public class TeacherService : ITeacherService
 {
-    
+    private readonly AppDbContext _context;
+    public TeacherService()
+    {
+        _context = new AppDbContextFactory().CreateDbContext();
+    }
+    public void Create(string firstName,string lastName)
+    {
+        _context.Teachers.Add(new Teacher() {FirstName = firstName,LastName = lastName });
+        _context.SaveChanges();
+    }
+
+    public void Update(int id, string firstName,string lastName)
+    {
+        var teacher = _context.Teachers.FirstOrDefault(x => x.Id == id);
+        if (teacher == null) throw new Exception("Null reference");
+        teacher.FirstName = firstName;
+        teacher.LastName = lastName;
+        _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        var teacher = _context.Teachers.FirstOrDefault(x => x.Id == id);
+        if (teacher == null) throw new Exception("Null reference");
+        _context.Teachers.Remove(teacher);
+        _context.SaveChanges();
+    }
+
+    public Teacher GetById(int id)
+    {
+        var teacher = _context.Teachers.FirstOrDefault(x => x.Id == id);
+        if (teacher == null) throw new Exception("Null reference");
+        return teacher;
+    }
+
+    public List<Teacher> GetAll()
+    {
+        var teachers = _context.Teachers.ToList();
+        return teachers;
+    }
 }
