91384feb57259c4f5c73e953f7262a3b6345b7a8
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 19:31:57 +0200

    Add AddChildren Window

---
+++ WPFApp/Windows/AddChildren.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using EFLib.Services;
+
+namespace WPFApp.Windows;
+
+public partial class AddChildren : Window
+{
+    private readonly IChildrenService _service;
+    public AddChildren()
+    {
+        InitializeComponent();
+        _service = new ChildrenService();
+    }
+
+
+    private void AddChildren_Button(object sender, RoutedEventArgs e)
+    {
+        if (FirstName != null && LastName != null && ParentId != null && TeacherId != null)
+        {
+            _service.Create(FirstName.Text, LastName.Text, Int32.Parse(ParentId.Text), Int32.Parse(TeacherId.Text));
+            MessageBox.Show("Added succesfully!");
+            this.Close();
+        }
+        else
+        {
+            MessageBox.Show("Invalid input!");
+        }
+    }
+}
