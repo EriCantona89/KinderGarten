b86494cb9ed30d394f677dd58f0e2782ffa706b9
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 18:35:11 +0200

    Add PresentService

--- EFLib/Services/PresentService.cs
+++ EFLib/Services/PresentService.cs
@@ -1,6 +1,50 @@
-﻿namespace EFLib.Services;
+﻿using EFLib.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFLib.Services;
 
 public class PresentService
 {
+    private readonly AppDbContext _context;
+    public PresentService()
+    {
+        _context = new AppDbContextFactory().CreateDbContext();
+    }
     
+    public void Create(DateTime date,int childrenId,bool present)
+    {
+        _context.Presents.Add(new Present() {Date = date, ChildrenId = childrenId, Presents = present});
+        _context.SaveChanges();
+    }
+
+    public void Update(int id,DateTime date,int childrenId,bool presents)
+    {
+        var present = _context.Presents.FirstOrDefault(x => x.Id == id);
+        if (present == null) throw new Exception("Null reference");
+        present.Date = date;
+        present.ChildrenId = childrenId;
+        present.Presents = presents;
+        _context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        var present = _context.Presents.FirstOrDefault(x => x.Id == id);
+        if (present == null) throw new Exception("Null reference");
+        _context.Presents.Remove(present);
+        _context.SaveChanges();
+    }
+
+    public Present GetById(int id)
+    {
+        var present = _context.Presents.FirstOrDefault(x => x.Id == id);
+        if (present == null) throw new Exception("Null reference");
+        return present;
+    }
+
+    public List<Present> GetAll()
+    {
+        var presents = _context.Presents.Include(x=>x.Children).ToList();
+        return presents;
+    }
 }
