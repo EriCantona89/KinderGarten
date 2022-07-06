b9ab13b65b4941a05825c72300a4ee53a9c915fe
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 19:28:11 +0200

    Add AddParent Window

---
+++ WPFApp/Windows/AddParent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using EFLib.Services;
+
+namespace WPFApp.Windows;
+
+public partial class AddParent : Window
+{
+    private readonly IParentService _service;
+    public AddParent()
+    {
+        InitializeComponent();
+        _service = new ParentService();
+    }
+
+    private void AddTeacher_Button(object sender, RoutedEventArgs e)
+    {
+        if (FirstName != null && LastName != null )
+        {
+            _service.Create(FirstName.Text, LastName.Text);
+            MessageBox.Show("Added successfully!");
+            this.Close();
+        }
+        else
+        {
+            MessageBox.Show("Invalid input!");
+        }
+    }
+}
