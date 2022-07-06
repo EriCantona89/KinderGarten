b82f55d812f694708b4c93ef94267692c238595e
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 19:17:45 +0200

    Add TeacherView

--- WPFApp/Views/TeacherView.xaml.cs
+++ WPFApp/Views/TeacherView.xaml.cs
@@ -1,11 +1,47 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using EFLib.Services;
 
 namespace WPFApp.Views;
 
 public partial class TeacherView : UserControl
 {
+    private readonly ITeacherService _service;
     public TeacherView()
     {
         InitializeComponent();
+        _service = new TeacherService();
+        GetAll();
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        dynamic entity = ((Button) sender).DataContext;
+        _service.Delete(entity.Id);
+        GetAll();
+    }
+
+    private void Update_Click(object sender, RoutedEventArgs e)
+    {
+        // TO DO Window
+        throw new System.NotImplementedException();
+    }
+
+    private void AddTeacher_Click(object sender, RoutedEventArgs e)
+    {
+        // TO DO Window
+        throw new System.NotImplementedException();
+    }
+
+    private void Refresh_Click(object sender, RoutedEventArgs e)
+    {
+        GetAll();
+    }
+
+    public  void GetAll()
+    {
+        var list =  _service.GetAll();
+        View.ItemsSource = list;
+
     }
 }
