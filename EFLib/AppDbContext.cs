dfd88c1060f34fd00497abf726ebe8dfc3799a54
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 18:11:47 +0200

    Add AppDbContext and AppDbContextFactory

---
+++ EFLib/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFLib;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext()
+    {
+    }
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+}
---
+++ EFLib/AppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EFLib;
+
+public class AppDbContextFactory:IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args = null)
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>();
+        options.UseSqlServer("Server=.; Database=KinderGarten; Trusted_Connection=True");
+        return new AppDbContext(options.Options);
+
+    }
+}
