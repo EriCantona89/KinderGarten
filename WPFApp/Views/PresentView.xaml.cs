ea8039995b941b92425adde2b2cf3b0dafe436ad
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 19:14:48 +0200

    Add PresentView

--- WPFApp/Views/PresentView.xaml.cs
+++ WPFApp/Views/PresentView.xaml.cs
@@ -1,11 +1,47 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using EFLib.Services;
 
 namespace WPFApp.Views;
 
 public partial class PresentView : UserControl
 {
+    private readonly IPresentService _service;
     public PresentView()
     {
         InitializeComponent();
+        _service = new PresentService();
+        GetAll();
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        dynamic entity = ((Button) sender).DataContext;
+        _service.Delete(entity.Id);
+        GetAll();
+    }
+
+    private void Update_Click(object sender, RoutedEventArgs e)
+    {
+        // TO DO Window
+        throw new System.NotImplementedException();
+    }
+
+    private void AddPresent_Click(object sender, RoutedEventArgs e)
+    {
+        // TO DO Window
+        throw new System.NotImplementedException();
+    }
+
+    private void Refresh_Click(object sender, RoutedEventArgs e)
+    {
+        GetAll();
+    }
+
+    public  void GetAll()
+    {
+        var list =  _service.GetAll();
+        View.ItemsSource = list;
+
     }
 }
