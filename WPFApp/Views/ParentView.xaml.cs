fe77902ff361ab0b1f1f81cffc703111f0332ab8
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 19:08:18 +0200

    Add ParentView

--- WPFApp/Views/ParentView.xaml.cs
+++ WPFApp/Views/ParentView.xaml.cs
@@ -1,11 +1,47 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using EFLib.Services;
 
 namespace WPFApp.Views;
 
 public partial class ParentView : UserControl
 {
+    private readonly IParentService _service;
     public ParentView()
     {
         InitializeComponent();
+        _service = new ParentService();
+        GetAll();
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        dynamic entity = ((Button) sender).DataContext;
+        _service.Delete(entity.Id);
+        GetAll();
+    }
+
+    private void Update_Click(object sender, RoutedEventArgs e)
+    {
+        // TO DO Window
+        throw new System.NotImplementedException();
+    }
+
+    private void AddParent_Click(object sender, RoutedEventArgs e)
+    {
+        // TO DO Window
+        throw new System.NotImplementedException();
+    }
+
+    private void Refresh_Click(object sender, RoutedEventArgs e)
+    {
+        GetAll();
+    }
+
+    public  void GetAll()
+    {
+        var list =  _service.GetAll();
+        View.ItemsSource = list;
+
     }
 }
