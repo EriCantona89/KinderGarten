d1443e4a788f8677a91489a13e7a52811b88a3f9
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 18:19:44 +0200

    Add sets to AppDbContext

--- EFLib/AppDbContext.cs
+++ EFLib/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFLib.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFLib;
 
@@ -10,5 +11,19 @@ public class AppDbContext:DbContext
     public AppDbContext(DbContextOptions options) : base(options)
     {
     }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Children>().HasKey(x => x.Id);
+        modelBuilder.Entity<Teacher>().HasKey(x => x.Id);
+        modelBuilder.Entity<Present>().HasKey(x => x.Id);
+        modelBuilder.Entity<Parent>().HasKey(x => x.Id);
+    }
+    
+    public DbSet<Children> Childrens { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Present> Presents { get; set; }
+    public DbSet<Parent> Parents { get; set; }
+
     
 }
