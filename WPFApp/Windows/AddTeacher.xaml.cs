40cc79d00d33cc79d1ef414aeab9b1cb80fe63c9
Kuba Sochacki <kuba.soch@hotmail.com>
2022-07-06 19:26:19 +0200

    Add AddTeacher Window

---
+++ WPFApp/Windows/AddTeacher.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using EFLib.Services;
+
+namespace WPFApp.Windows;
+
+public partial class AddTeacher : Window
+{
+    private readonly ITeacherService _service;
+    public AddTeacher()
+    {
+        InitializeComponent();
+        _service = new TeacherService();
+    }
+
+    private void AddTeacher_Button(object sender, RoutedEventArgs e)
+    {
+        if (FirstName != null && LastName != null )
+        {
+            _service.Create(FirstName.Text, LastName.Text);
+            MessageBox.Show("Added successfully!");
+            this.Close();
+        }
+        else
+        {
+            MessageBox.Show("Invalid input!");
+        }
+    }
+}
